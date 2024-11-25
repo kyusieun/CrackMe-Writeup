7be076d44281a7a88f04c25cd0e7c7ade7d8cdb7
kyusieun <kyusieun@gmail.com>
2024-11-25 16:57:46 +0900

    Easy Keygen

---
+++ reversing.kr/Easy_KeygenMe/solve.py
@@ -0,0 +1,9 @@
+xorKey = 0x1020301020301020
+Serial = 0x5B134977135E7D13
+
+# Serial에 xor_key를 xor 연산한 결과를 출력한다.
+xor = Serial ^ xorKey
+print(hex(xor))
+
+# xor을 ascii로 변환한다.
+print(bytes.fromhex(hex(xor)[2:]).decode())
